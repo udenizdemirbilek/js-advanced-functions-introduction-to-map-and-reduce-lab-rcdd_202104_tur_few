58b0ac990fb659dff00c432d981bac88ae153e91
demirbileku <udemirbilek2@gmail.com>
2021-05-14 16:03:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    const output = []
+    sourceArray.forEach(element => {
+        output.push(element*-1)        
+    });
+    return output;
+}
+
+function mapToNoChange(sourceArray) {
+    const output = []
+    sourceArray.forEach(element => {
+        output.push(element)        
+    });
+    return output;
+}
+
+function mapToDouble(sourceArray) {
+    const output = []
+    sourceArray.forEach(element => {
+        output.push(element*2)        
+    });
+    return output;
+}
+
+function mapToSquare(sourceArray) {
+    const output = []
+    sourceArray.forEach(element => {
+        output.push(element**2)        
+    });
+    return output;
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    sourceArray.forEach(element => {
+        startingPoint += element;
+    });
+    return startingPoint;
+}
+
+function reduceToAllTrue(sourceArray) {
+    let output = true;
+    sourceArray.forEach(element => {
+        if(element){
+        } else {
+            output = false;
+            // return output
+        }
+    });
+    return output;
+}
+
+function reduceToAnyTrue(sourceArray) {
+    let output = false;
+    sourceArray.forEach(element => {
+        if(element){
+            output = true;
+            // return output
+        } else {}
+    });
+    return output;
+}
